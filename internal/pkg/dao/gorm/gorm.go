33e987785d3e8df984c426853703bce1525753fb
WengTina <tina.weng8@gmail.com>
2023-09-22 15:05:47 +0800

    feat: 新增帳號管理與驗證

--- internal/pkg/dao/gorm/gorm.go
+++ internal/pkg/dao/gorm/gorm.go
@@ -1,64 +1,73 @@
 package gorm
 
 import (
-	"fmt"
-	"os"
-	"strconv"
+	//"fmt"
+	//"os"
+	//"strconv"
 
-	"eirc.app/internal/pkg/log"
+	//"eirc.app/internal/pkg/log"
 	"gorm.io/driver/sqlite"
 	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
-	"gorm.io/plugin/dbresolver"
+	//"gorm.io/plugin/dbresolver"
 )
 
 func New() (*gorm.DB, error) {
-	const config string = "server=%s;user id=%s;password=%s;port=%s;database=%s;sslmode=%s"
-	sources := fmt.Sprintf(config,
-		os.Getenv("SOURCES_HOST"),
-		os.Getenv("SOURCES_USER"),
-		os.Getenv("SOURCES_PASSWORD"),
-		os.Getenv("SOURCES_PORT"),
-		os.Getenv("SOURCES_DATABASE"),
-		os.Getenv("SOURCES_SSLMODE"),
-	)
-	db, err := gorm.Open(sqlserver.Open(sources), &gorm.Config{
-		DisableForeignKeyConstraintWhenMigrating: true,
-	})
+	dsn := "sqlserver://ManuOrder:nkuste215@163.18.42.223:1433?database=XMLY5000"
+	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
-
-	dbReplicas, err := strconv.Atoi(os.Getenv("DB_REPLICAS"))
-	if err != nil {
-		log.Debug(err)
-	}
-	if dbReplicas >= 1 {
-		var dialectics []gorm.Dialector
-		for i := 1; i <= dbReplicas; i++ {
-			replicas := fmt.Sprintf(config,
-				os.Getenv("REPLICAS_HOST_"+strconv.Itoa(i)),
-				os.Getenv("REPLICAS_USER_"+strconv.Itoa(i)),
-				os.Getenv("REPLICAS_PASSWORD_"+strconv.Itoa(i)),
-				os.Getenv("REPLICAS_PORT_"+strconv.Itoa(i)),
-				os.Getenv("REPLICAS_DATABASE_"+strconv.Itoa(i)),
-				os.Getenv("REPLICAS_SSLMODE_"+strconv.Itoa(i)),
-			)
-			director := sqlserver.Open(replicas)
-			dialectics = append(dialectics, director)
-		}
-		err = db.Use(
-			dbresolver.Register(dbresolver.Config{
-				Replicas: dialectics,
-			}),
-		)
-		if err != nil {
-			log.Error(err)
-		}
-	}
-
 	return db, nil
 }
+
+// func New() (*gorm.DB, error) {
+// 	//const config string = "server=%s;user id=%s;password=%s;port=%s;database=%s;sslmode=%s"
+// 	sources := fmt.Sprintf(config,
+// 		os.Getenv("SOURCES_HOST"),
+// 		os.Getenv("SOURCES_USER"),
+// 		os.Getenv("SOURCES_PASSWORD"),
+// 		os.Getenv("SOURCES_PORT"),
+// 		os.Getenv("SOURCES_DATABASE"),
+// 		os.Getenv("SOURCES_SSLMODE"),
+// 	)
+// 	db, err := gorm.Open(sqlserver.Open(sources), &gorm.Config{
+// 		DisableForeignKeyConstraintWhenMigrating: true,
+// 	})
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	dbReplicas, err := strconv.Atoi(os.Getenv("DB_REPLICAS"))
+// 	if err != nil {
+// 		log.Debug(err)
+// 	}
+// 	if dbReplicas >= 1 {
+// 		var dialectics []gorm.Dialector
+// 		for i := 1; i <= dbReplicas; i++ {
+// 			replicas := fmt.Sprintf(config,
+// 				os.Getenv("REPLICAS_HOST_"+strconv.Itoa(i)),
+// 				os.Getenv("REPLICAS_USER_"+strconv.Itoa(i)),
+// 				os.Getenv("REPLICAS_PASSWORD_"+strconv.Itoa(i)),
+// 				os.Getenv("REPLICAS_PORT_"+strconv.Itoa(i)),
+// 				os.Getenv("REPLICAS_DATABASE_"+strconv.Itoa(i)),
+// 				os.Getenv("REPLICAS_SSLMODE_"+strconv.Itoa(i)),
+// 			)
+// 			director := sqlserver.Open(replicas)
+// 			dialectics = append(dialectics, director)
+// 		}
+// 		err = db.Use(
+// 			dbresolver.Register(dbresolver.Config{
+// 				Replicas: dialectics,
+// 			}),
+// 		)
+// 		if err != nil {
+// 			log.Error(err)
+// 		}
+// 	}
+
+// 	return db, nil
+// }
 
 func NewSQLite() (*gorm.DB, error) {
 	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
--- main.go
+++ main.go
@@ -8,10 +8,13 @@ import (
 	"eirc.app/internal/pkg/dao/gorm"
 	"eirc.app/internal/pkg/log"
 	"eirc.app/internal/v1/router"
+	routeAccount "eirc.app/internal/v1/router/account"
 	routerCustomer "eirc.app/internal/v1/router/customer"
 	routerFile "eirc.app/internal/v1/router/file"
+	routerLogin "eirc.app/internal/v1/router/login"
 	routerSalesInfo "eirc.app/internal/v1/router/sales_info"
 
+	AccountModel "eirc.app/internal/v1/structure/accounts"
 	fileModel "eirc.app/internal/v1/structure/file"
 )
 
@@ -33,11 +36,12 @@ func main() {
 		return
 	}
 	db.AutoMigrate(&fileModel.Table{})
-
+	db.AutoMigrate(&AccountModel.Table{})
 	route := router.Default()
 	route = routerCustomer.GetRoute(route, dbLY)      //客戶路由
 	route = routerSalesInfo.GetRoute(route, dbLY, db) //銷貨單路由
 	route = routerFile.GetRoute(route, db, dbLY)      //檔案上傳路由
-
-	log.Fatal(http.ListenAndServe("172.29.160.1:8090", route))
+	route = routerLogin.GetRoute(route, db)
+	route = routeAccount.GetRoute(route, db)
+	log.Fatal(http.ListenAndServe("192.168.50.254:8090", route))
 }
